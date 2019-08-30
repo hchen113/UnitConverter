92c9a2bcd2f641cb97bc2d072db85a1970fe4fc3
Henry Chen <hchen113@binghamton.edu>
2019-08-30 11:59:36 -0400

    08/30
    Added convert button and input edit text line

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -21,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton convert_button = findViewById(R.id.convert_button);
+        convert_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
