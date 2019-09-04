4d81c30300a13057d9e47ab67fed41a463d775dc
Henry Chen <hchen113@binghamton.edu>
2019-09-04 10:42:02 -0400

    finished CM to IN

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -11,6 +11,9 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,12 +24,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        final EditText text_input = findViewById(R.id.text_input);
         FloatingActionButton convert_button = findViewById(R.id.convert_button);
+        final TextView text_output = findViewById(R.id.text_output);
+
         convert_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                double cm_val = Double.parseDouble(text_input.getText().toString());
+                double in_val = cm_val/2.54;
+                int ft_val = (int) in_val/12;
+                double ft_remain_in = in_val - (ft_val*12);
+
+                text_output.setText(String.valueOf(df2.format(in_val)) + " inches" + "\n" + ft_val + " feet " + ft_remain_in + " in");
+
             }
         });
     }
